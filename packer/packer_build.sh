334319a21098620940c8bd7f2cdfaf974c4c1f07
pasitconsulting <pasitconsulting@outlook.com>
2019-07-30 10:14:14 +0000

    removed secrets from code

--- packer/packer_build.sh
+++ packer/packer_build.sh
@@ -1,4 +1,4 @@
 /etc/ansible/packer/packer build \
-    -var 'aws_access_key=AKIAT6BJOHYMONPNHWB6' \
-    -var 'aws_secret_key=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' \
+    -var 'aws_access_key=' \
+    -var 'aws_secret_key=' \
     template.json
